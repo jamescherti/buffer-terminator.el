66346603d9e9ebe1805086a3527ba498d0b6347d
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-12-07 17:44:31 -0500

    Update unit-tests

--- tests/test-buffer-terminator.el
+++ tests/test-buffer-terminator.el
@@ -366,7 +366,7 @@
   (test-buffer-terminator--check-modified-file-buffer t)
   (test-buffer-terminator--check-process-buffer nil))
 
-(ert-deftest test-buffer-terminator-test10-mode ()
+(ert-deftest test-buffer-terminator-test10-mode-kill ()
   ;; Kill all of them
   (test-buffer-terminator--create-test-environment)
   (setq buffer-terminator-rules-alist
@@ -377,14 +377,14 @@
   (unwind-protect
       (progn
         (buffer-terminator-mode 1)
-        (sleep-for 2)
+        (sleep-for 1.5)
         (test-buffer-terminator--check-special-buffers nil)
         (test-buffer-terminator--check-special-mode-buffer nil)
         (test-buffer-terminator--check-func-buffer nil)
         (test-buffer-terminator--check-file-buffers nil)
         (test-buffer-terminator--check-modified-file-buffer t)
         (test-buffer-terminator--check-process-buffer nil))
-    (buffer-terminator-mode 1)))
+    (buffer-terminator-mode 0)))
 
 (provide 'test-buffer-terminator)
 ;;; test-buffer-terminator.el ends here
